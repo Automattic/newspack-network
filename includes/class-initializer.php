d1158de4eff192de273ca4b83ed583d3a4fccbbe
Leo Germani <leogermani@automattic.com>
2023-05-04 18:29:44 -0300

    bootstrapping sites admin

---
+++ includes/class-initializer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Newspack Hub plugin initialization.
+ *
+ * @package Newspack
+ */
+
+namespace Newspack_Hub;
+
+/**
+ * Class to handle the plugin initialization
+ */
+class Initializer {
+
+	/**
+	 * Runs the initialization.
+	 */
+	public static function init() {
+		Admin::init();
+	}
+
+}
