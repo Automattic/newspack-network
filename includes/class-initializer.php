c47cb15efb52d6a4470f85e5e1df9a1898faa2b6
Miguel Peixe <miguel.peixe@automattic.com>
2023-12-01 10:18:22 -0300

    fix: remove gam ad targeting (#16)

--- includes/class-initializer.php
+++ includes/class-initializer.php
@@ -26,6 +26,7 @@ class Initializer {
 			Hub\Event_Listeners::init();
 			Hub\Database\Subscriptions::init();
 			Hub\Database\Orders::init();
+			Hub\Newspack_Ads_GAM::init();
 		}
 
 		if ( Site_Role::is_node() ) {
@@ -41,7 +42,6 @@ class Initializer {
 		Reader_Roles_Filter::init();
 		User_Update_Watcher::init();
 		Distributor_Customizations::init();
-		GAM::init();
 
 		register_activation_hook( NEWSPACK_NETWORK_PLUGIN_FILE, [ __CLASS__, 'activation_hook' ] );
 	}
