00c4da889eae623339396e380fbcc7ed134ca666
Leo Germani <leogermani@automattic.com>
2023-12-19 17:00:59 -0300

    fix: remove duplicate require

--- includes/utils/class-users.php
+++ includes/utils/class-users.php
@@ -93,7 +93,6 @@ class Users {
 			if ( ! function_exists( 'media_sideload_image' ) ) {
 				require_once ABSPATH . 'wp-admin/includes/media.php';
 				require_once ABSPATH . 'wp-admin/includes/file.php';
-				require_once ABSPATH . 'wp-admin/includes/media.php';
 				require_once ABSPATH . 'wp-admin/includes/image.php';
 			}
 
