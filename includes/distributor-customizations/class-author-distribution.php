eeb2568d83fa44f99f734493d2257f9e37636ad8
Leo Germani <leogermani@automattic.com>
2023-12-19 15:14:56 -0300

    feat: update authors on post update

--- includes/distributor-customizations/class-author-distribution.php
+++ includes/distributor-customizations/class-author-distribution.php
@@ -26,8 +26,11 @@ class Author_Distribution {
 	 */
 	public static function init() {
 		add_filter( 'dt_push_post_args', [ __CLASS__, 'add_author_data_to_push' ], 10, 2 );
+		add_filter( 'dt_subscription_post_args', [ __CLASS__, 'add_author_data_to_push' ], 10, 2 );
 		add_filter( 'rest_prepare_post', [ __CLASS__, 'add_author_data_to_pull' ], 10, 3 );
 		add_filter( 'dt_syncable_taxonomies', [ __CLASS__, 'filter_syncable_taxonomies' ] );
+
+		add_filter( 'rest_request_after_callbacks', [ __CLASS__, 'after_coauthors_update' ], 10, 3 );
 	}
 
 	/**
@@ -50,6 +53,9 @@ class Author_Distribution {
 
 	/**
 	 * Filters the post data sent on a push to add the author data.
+	 *
+	 * This callback is also used to add data to the post sent to the subscription endpoint.
+	 * (sends an update to the linked posts in other sites)
 	 *
 	 * @param array   $post_body The post data.
 	 * @param WP_Post $post The post object.
@@ -216,6 +222,41 @@ class Author_Distribution {
 		}
 
 		return $author;
+	}
+
+	/**
+	 * Sends an extra notification to subscribers when the authors of a post are updated.
+	 *
+	 * CoAuthors Plus updates the authors through an additional ajax request in the editor after the post is updated,
+	 * therefore when we change the authors and update the post in the Editor, the notification sent to the subscribers still
+	 * has the old authors
+	 *
+	 * We don't filter the response here, we just send the notification.
+	 *
+	 * @param WP_REST_Response|WP_HTTP_Response|WP_Error|mixed $response Result to send to the client.
+	 * @param array                                            $handler  Route handler used for the request.
+	 * @param WP_REST_Request                                  $request  Request used to generate the response.
+	 * @return WP_REST_Response|WP_HTTP_Response|WP_Error|mixed
+	 */
+	public static function after_coauthors_update( $response, $handler, $request ) {
+
+		if ( ! class_exists( 'CoAuthors\API\Endpoints' ) || ! function_exists( 'Distributor\Subscriptions\send_notifications' ) ) {
+			return $response;
+		}
+
+		$coauthors_endpoint_base = \CoAuthors\API\Endpoints::NS . '/' . \CoAuthors\API\Endpoints::AUTHORS_ROUTE;
+
+		if ( false === strpos( $request->get_route(), $coauthors_endpoint_base ) ) {
+			return $response;
+		}
+
+		if ( 'POST' !== $request->get_method() ) {
+			return $response;
+		}
+
+		\Distributor\Subscriptions\send_notifications( (int) $request->get_param( 'post_id' ) );
+
+		return $response;
 	}
 
 }
