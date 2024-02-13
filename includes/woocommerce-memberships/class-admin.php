adf3446927299dcc49a2de5802242eb84a2c0ece
Leo Germani <leogermani@automattic.com>
2024-02-13 15:43:01 -0300

    feat: redirect distributed membership edit link

--- includes/woocommerce-memberships/class-admin.php
+++ includes/woocommerce-memberships/class-admin.php
@@ -34,6 +34,8 @@ class Admin {
 	public static function init() {
 		add_action( 'add_meta_boxes', array( __CLASS__, 'add_meta_box' ), 100 ); // Memberships plugin does something there and we need a higher priority.
 		add_action( 'save_post', array( __CLASS__, 'save_meta_box' ) );
+		add_filter( 'get_edit_post_link', array( __CLASS__, 'get_edit_post_link' ), 10, 2 );
+		add_filter( 'post_row_actions', array( __CLASS__, 'post_row_actions' ), 99, 2 ); // After the Memberships plugin.
 	}
 
 	/**
@@ -96,5 +98,45 @@ class Admin {
 
 		$network_id = sanitize_text_field( wp_unslash( $_POST['newspack_network_id'] ?? '' ) );
 		update_post_meta( $post_id, self::NETWORK_ID_META_KEY, $network_id );
+	}
+
+	/**
+	 * Filters the edit post link for user memberships managed in another site of the network.
+	 *
+	 * @param string $link The edit post link.
+	 * @param int    $post_id The post ID.
+	 * @return string
+	 */
+	public static function get_edit_post_link( $link, $post_id ) {
+		$managed_by_newspack_network = get_post_meta( $post_id, '_managed_by_newspack_network', true );
+		if ( $managed_by_newspack_network ) {
+			$link = str_replace( get_bloginfo( 'url' ), $managed_by_newspack_network, $link );
+		}
+		return $link;
+	}
+
+	/**
+	 * Filters the row actions for user memberships managed in another site of the network.
+	 *
+	 * @param string[] $actions An array of row action links. Defaults are
+	 *                          'Edit', 'Quick Edit', 'Restore', 'Trash',
+	 *                          'Delete Permanently', 'Preview', and 'View'.
+	 * @param WP_Post  $post    The post object.
+	 * @return array
+	 */
+	public static function post_row_actions( $actions, $post ) {
+		$managed_by_newspack_network = get_post_meta( $post->ID, '_managed_by_newspack_network', true );
+		if ( ! $managed_by_newspack_network ) {
+			return $actions;
+		}
+		$link     = get_edit_post_link( $post->ID );
+		$link_tag = sprintf( '<a href="%s" target="_blank">%s</a>', $link, $managed_by_newspack_network );
+		return [
+			'none' => sprintf(
+				// translators: %s is the site URL with a link to edit the post.
+				__( 'Managed in %s', 'newspack-network' ),
+				$link_tag
+			),
+		];
 	}
 }
