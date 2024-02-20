7475bb9b1684d6cd40f02bba819e313b7c9e66b9
matticbot <semantic-release-bot@martynus.net>
2024-02-20 18:20:56 +0000

    chore(release): 1.2.0 [skip ci]
    # [1.2.0](https://github.com/Automattic/newspack-network/compare/v1.1.0...v1.2.0) (2024-02-20)
    
    ### Bug Fixes
    
    * assorted error handling fixes ([#52](https://github.com/Automattic/newspack-network/issues/52)) ([234f883](https://github.com/Automattic/newspack-network/commit/234f883716dab7ce9ba2bac8a2bf93e58bbeb887))
    * **data-listeners:** handle no user in wc data update ([4eeefc0](https://github.com/Automattic/newspack-network/commit/4eeefc088b12ffdb17db669d9b1f6605c546d1b1))
    * dynamic class property deprecated warnings ([#47](https://github.com/Automattic/newspack-network/issues/47)) ([693865c](https://github.com/Automattic/newspack-network/commit/693865c71ea18e3b3aff2810cc61ec407144dd43))
    * prevent coauthors capability check infinite loop ([#46](https://github.com/Automattic/newspack-network/issues/46)) ([9565cef](https://github.com/Automattic/newspack-network/commit/9565cefeedd7cfc518b35dd84cce21b171664781))
    * race condition when creating a Node ([1946473](https://github.com/Automattic/newspack-network/commit/1946473d0d1baf2bef2c66b5377e0e48b630df75))
    * set Yoast primary category ([#41](https://github.com/Automattic/newspack-network/issues/41)) ([3457d19](https://github.com/Automattic/newspack-network/commit/3457d19ab5aea56d13ea2f7179dd1296d563ae7e))
    
    ### Features
    
    * **ci:** add epic/* release workflow and rename `master` to `trunk` ([#39](https://github.com/Automattic/newspack-network/issues/39)) ([9cee51d](https://github.com/Automattic/newspack-network/commit/9cee51dfd407919631df144b11e9599a312cffce))
    * sync billing and shipping addresses ([#50](https://github.com/Automattic/newspack-network/issues/50)) ([6a05580](https://github.com/Automattic/newspack-network/commit/6a055808f244ec2ad790a13d068431feb6acdc17))
    * sync publish, trash post statuses ([#42](https://github.com/Automattic/newspack-network/issues/42)) ([fd5d8b9](https://github.com/Automattic/newspack-network/commit/fd5d8b9d2c613486a43f061df1caf73bd6fb3979))

--- newspack-network.php
+++ newspack-network.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Network
  * Description: The Newspack Network plugin.
- * Version: 1.2.0-alpha.3
+ * Version: 1.2.0
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
