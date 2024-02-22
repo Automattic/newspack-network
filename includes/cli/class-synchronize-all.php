c6d1d8dc3c55071fb2943309a53693e4a1319d78
Leo Germani <leogermani@automattic.com>
2024-02-22 16:43:40 -0300

    feat: sync all and sync check commands

--- includes/cli/class-synchronize-all.php
+++ includes/cli/class-synchronize-all.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Data Backfill scripts.
+ * Sync all scripts.
  *
  * @package Newspack
  */
@@ -8,11 +8,13 @@
 namespace Newspack_Network;
 
 use WP_CLI;
+use Newspack_Network\Node\Pulling;
 
 /**
- * Data Backfill class.
+ * Sync all class.
  */
 class Synchronize_All {
+
 	/**
 	 * Number of events left to fetch.
 	 *
@@ -37,14 +39,39 @@ class Synchronize_All {
 	public static function register_commands() {
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			WP_CLI::add_command( 'newspack-network sync-all', [ __CLASS__, 'sync_all' ] );
+			WP_CLI::add_command( 'newspack-network sync-check', [ __CLASS__, 'sync_check' ] );
 		}
 	}
 
 	/**
-	 * Get events from the Hub.
+	 * Process events received from the Hub.
 	 */
-	private static function get_events() {
-		$response = \Newspack_Network\Node\Pulling::make_request();
+	private static function process_events() {
+
+		$response = self::make_request();
+		WP_CLI::line( 'Received ' . count( $response['data'] ) . ' events, processing…' );
+
+		Pulling::process_pulled_data( $response['data'] );
+
+		self::$events_left = (int) $response['total'] - \Newspack_Network\Hub\Pull_Endpoint::get_pull_limit();
+		if ( 0 > self::$events_left ) {
+			self::$events_left = 0;
+		}
+		WP_CLI::line( 'Events left to fetch: ' . self::$events_left );
+
+		if ( self::$events_left > 0 ) {
+			self::process_events();
+		}
+		WP_CLI::success( 'Sync complete.' );
+	}
+
+	/**
+	 * Makes a request to pull events from the Hub
+	 *
+	 * @return array
+	 */
+	private static function make_request() {
+		$response = Pulling::make_request();
 		if ( is_wp_error( $response ) ) {
 			WP_CLI::error( 'Error fetching events from the Hub: ' . $response->get_error_message() );
 		}
@@ -52,27 +79,42 @@ class Synchronize_All {
 		if ( ! isset( $response['data'] ) ) {
 			WP_CLI::error( 'Missing data in response.' );
 		}
-
-		WP_CLI::line( 'Received ' . count( $response['data'] ) . ' events, processing…' );
-
-		\Newspack_Network\Node\Pulling::process_pulled_data( $response['data'] );
-
-		self::$events_left = $response['more_items_count'];
-		WP_CLI::line( 'Events left to fetch: ' . self::$events_left );
-
-		if ( self::$events_left > 0 ) {
-			self::get_events();
-		}
+		return $response;
 	}
 
 	/**
-	 * Sync all data.
+	 * Syncs all data, pulling all events from the Hub.
 	 */
 	public static function sync_all() {
 		WP_CLI::line( '' );
 		if ( ! Site_Role::is_node() ) {
 			WP_CLI::error( 'This command can only be run on a Node site.' );
 		}
-		self::get_events();
+		WP_CLI::line( 'Syncing all data from the Hub can take a long time and will write data to the site.' );
+		WP_CLI::line( 'Consider running wp newspack-network sync-check first to check the size of the queue.' );
+		WP_CLI::confirm( 'Are you sure you want to sync all data?' );
+		self::process_events();
+	}
+
+	/**
+	 * Checks the current state of the sync
+	 */
+	public static function sync_check() {
+		WP_CLI::line( '' );
+		if ( ! Site_Role::is_node() ) {
+			WP_CLI::error( 'This command can only be run on a Node site.' );
+		}
+		WP_CLI::line( 'Checking the sync queue' );
+
+		$response = self::make_request();
+		$events_on_the_hub = (int) $response['total'];
+		$last_processed_id = Pulling::get_last_processed_id();
+
+		WP_CLI::line( 'Last processed event ID: ' . $last_processed_id );
+		if ( $events_on_the_hub === 0 ) {
+			WP_CLI::success( 'Sync is up to date. Nothing to pull.' );
+		} else {
+			WP_CLI::line( 'Events left to sync: ' . ( $events_on_the_hub ) );
+		}
 	}
 }
