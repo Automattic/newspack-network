cf3df3b5d2d61f2a1679823f80c4780467a0f4f5
Adam Cassis <adam@adamcassis.com>
2024-04-25 16:22:03 +0200

    feat: handle user deletion event on the network (#91)

--- includes/class-data-listeners.php
+++ includes/class-data-listeners.php
@@ -36,6 +36,7 @@ class Data_Listeners {
 		Data_Events::register_listener( 'woocommerce_subscription_status_changed', 'newspack_node_subscription_changed', [ __CLASS__, 'item_changed' ] );
 		Data_Events::register_listener( 'woocommerce_order_status_changed', 'newspack_node_order_changed', [ __CLASS__, 'item_changed' ] );
 		Data_Events::register_listener( 'newspack_network_user_updated', 'network_user_updated', [ __CLASS__, 'user_updated' ] );
+		Data_Events::register_listener( 'delete_user', 'network_user_deleted', [ __CLASS__, 'user_deleted' ] );
 		Data_Events::register_listener( 'newspack_network_nodes_synced', 'network_nodes_synced', [ __CLASS__, 'nodes_synced' ] );
 	}
 
@@ -85,6 +86,28 @@ class Data_Listeners {
 	 */
 	public static function user_updated( $user_data ) {
 		return $user_data;
+	}
+
+	/**
+	 * Filters the user data for the event being triggered
+	 *
+	 * @param int      $id       ID of the user to delete.
+	 * @param int|null $reassign ID of the user to reassign posts and links to.
+	 *                           Default null, for no reassignment.
+	 * @param WP_User  $user     WP_User object of the user to delete.
+	 * @return array
+	 */
+	public static function user_deleted( $id, $reassign, $user ) {
+		$should_delete = apply_filters( 'newspack_network_process_user_deleted', true, $user->user_email );
+		if ( ! $should_delete ) {
+			Debugger::log( 'User deletion with email: ' . $user->user_email . ' was skipped due to filter use.' );
+			return;
+		}
+		// Prevent deletion-related changes triggering a 'network_user_updated' event.
+		User_Update_Watcher::$enabled = false;
+		return [
+			'email' => $user->user_email,
+		];
 	}
 
 	/**
