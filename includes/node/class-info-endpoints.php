08525a1e83514df58e577e2dfa0345e0d3976889
Adam Cassis <adam.cassis@automattic.com>
2024-07-16 10:46:47 +0200

    fix(api): improve /info endpoint

--- includes/node/class-info-endpoints.php
+++ includes/node/class-info-endpoints.php
@@ -7,9 +7,6 @@
 
 namespace Newspack_Network\Node;
 
-use Newspack_Network\Accepted_Actions;
-use Newspack_Network\Crypto;
-
 /**
  * Class that register the webhook endpoint that will send events to the Hub
  */
@@ -34,7 +31,9 @@ class Info_Endpoints {
 				[
 					'methods'             => \WP_REST_Server::READABLE,
 					'callback'            => [ __CLASS__, 'handle_info_request' ],
-					'permission_callback' => '__return_true',
+					'permission_callback' => function( $request ) {
+						return \Newspack_Network\Rest_Authenticaton::verify_signature( $request, 'info', Settings::get_secret_key() );
+					},
 				],
 			]
 		);
@@ -42,8 +41,10 @@ class Info_Endpoints {
 
 	/**
 	 * Handles the info request.
+	 *
+	 * @param WP_REST_Request $request The REST request received.
 	 */
-	public static function handle_info_request() {
+	public static function handle_info_request( $request ) {
 		return rest_ensure_response(
 			[
 				'sync_users_count' => \Newspack_Network\Utils\Users::get_synchronized_users_count(),
