e8d227c42fa6c7659def3cf60613aa2b155793bf
matticbot <semantic-release-bot@martynus.net>
2024-08-02 20:45:07 +0000

    chore(release): 2.0.0-alpha.1 [skip ci]
    # [2.0.0-alpha.1](https://github.com/Automattic/newspack-network/compare/v1.11.0-alpha.1...v2.0.0-alpha.1) (2024-08-02)
    
    ### Bug Fixes
    
    * update dependencies to support `@wordpress/scripts` ([#104](https://github.com/Automattic/newspack-network/issues/104)) ([e9691b8](https://github.com/Automattic/newspack-network/commit/e9691b8edcdc58cd3466bff46d2cbe4eacba9eaf))
    
    ### BREAKING CHANGES
    
    * Updates dependencies for compatibility with WordPress 6.6.*, but breaks JS in WordPress 6.5.* and below. If you need support for WP 6.5.*, please do not upgrade to this new major version.
    
    * chore: refactor for newspack-scripts dependency updates
    
    * chore: update composer
    
    * fix: peer dependencies
    
    * chore: update newspack-scripts to v5.6.0-alpha.3
    
    * chore: update newspack-scripts to v5.6.0-alpha.4
    
    * chore: remove unnecessary prettier config file
    
    * chore: update newspack-scripts to v5.6.0-alpha.7
    
    * fix: update phpcs.xml
    
    * chore: bump newspack-scripts to v5.5.2

--- newspack-network.php
+++ newspack-network.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Network
  * Description: The Newspack Network plugin.
- * Version: 1.11.0-alpha.1
+ * Version: 2.0.0-alpha.1
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
