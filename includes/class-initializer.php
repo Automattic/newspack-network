5206b7319cb23c4e23d0c6d6ca0470aafb603d45
Leo Germani <leogermani@automattic.com>
2024-08-07 12:46:45 -0300

    feat: limit subscription purchase for user with network membership

--- includes/class-initializer.php
+++ includes/class-initializer.php
@@ -58,6 +58,7 @@ class Initializer {
 		Woocommerce_Memberships\Admin::init();
 		Woocommerce_Memberships\Events::init();
 		Woocommerce_Memberships\Subscriptions_Integration::init();
+		Woocommerce_Memberships\Limit_Purchase::init();
 
 		Woocommerce\Events::init();
 
