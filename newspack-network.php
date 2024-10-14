368a6b671d6cd4dc37c840a6261820f813f0bbbf
matticbot <semantic-release-bot@martynus.net>
2024-10-14 18:14:23 +0000

    chore(release): 2.3.0-alpha.1 [skip ci]
    # [2.3.0-alpha.1](https://github.com/Automattic/newspack-network/compare/v2.2.1...v2.3.0-alpha.1) (2024-10-14)
    
    ### Bug Fixes
    
    * remove recursive import `use` ([b03bb52](https://github.com/Automattic/newspack-network/commit/b03bb52f702f873f465f8b0d62d935301933689d))
    
    ### Features
    
    * handle email change on propagated users ([#133](https://github.com/Automattic/newspack-network/issues/133)) ([2efa226](https://github.com/Automattic/newspack-network/commit/2efa2266dee3bb22d4b01ce8fbe0f0393e37cf2f))

--- newspack-network.php
+++ newspack-network.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Network
  * Description: The Newspack Network plugin.
- * Version: 2.2.1
+ * Version: 2.3.0-alpha.1
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
