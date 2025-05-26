be614cfda1006797b9b812fe5ac650d0f90e97a6
leogermani <leogermani@automattic.com>
2025-05-26 10:38:02 -0300

    feat: add support to yoast primary cat in content distribution (#241)
    * feat: add support to yoast primary cat in content distribution
    
    * fix: bail if yoast not in target site

---
+++ tests/unit-tests/content-distribution/test-yoast-primary-cat.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Class TestOutgoingPost
+ *
+ * @package Newspack_Network
+ */
+
+namespace Test\Content_Distribution;
+
+use Newspack_Network\Content_Distribution\Outgoing_Post;
+use Newspack_Network\Content_Distribution\Incoming_Post;
+use Newspack_Network\Content_Distribution\Yoast_Primary_Cat;
+
+require_once __DIR__ . '/mock-yoast-primary-term.php';
+
+/**
+ * Test the Outgoing_Post class.
+ */
+class TestYoastPrimaryCat extends \WP_UnitTestCase {
+
+	/**
+	 * Test category slug used in the test. It's part of the sample payload.
+	 */
+	const TEST_CATEGORY_SLUG = 'category-2';
+
+	/**
+	 * Test the outgoing post.
+	 */
+	public function test_outgoing_post() {
+		$post_id = self::factory()->post->create();
+
+		$category_id = self::factory()->category->create( [ 'slug' => self::TEST_CATEGORY_SLUG ] );
+
+		$primary_term = new \WPSEO_Primary_Term( 'category', $post_id );
+		$primary_term->set_primary_term( $category_id );
+
+		$outgoing_post = new Outgoing_Post( $post_id );
+
+		$outgoing_meta = $outgoing_post->get_payload()['post_data']['post_meta'];
+
+		$meta_name = Yoast_Primary_Cat::PRIMARY_CAT_SLUG_META_NAME;
+
+		$this->assertArrayHasKey( $meta_name, $outgoing_meta );
+		$this->assertEquals( self::TEST_CATEGORY_SLUG, $outgoing_meta[ $meta_name ][0], 'The primary category slug should be part of the outgoing post meta' );
+	}
+
+	/**
+	 * Test the incoming post.
+	 */
+	public function test_incoming_post() {
+		$payload = get_sample_payload( 'https://node1.test', 'https://node2.test' );
+
+		// Set the site URL for the node that receives posts.
+		update_option( 'siteurl', 'https://node2.test' );
+		update_option( 'home', 'https://node2.test' );
+
+		$meta_name = Yoast_Primary_Cat::PRIMARY_CAT_SLUG_META_NAME;
+
+		$payload['post_data']['post_meta'][ $meta_name ] = [ self::TEST_CATEGORY_SLUG ];
+
+		$incoming_post = new Incoming_Post( $payload );
+
+		$post_id = $incoming_post->insert();
+
+		$primary_term = new \WPSEO_Primary_Term( 'category', $post_id );
+		$primary_term_id = $primary_term->get_primary_term();
+
+		$primary_term_category = get_term( $primary_term_id, 'category' );
+
+		$this->assertEquals( self::TEST_CATEGORY_SLUG, $primary_term_category->slug, 'The primary category should be correctly set in the created post' );
+	}
+}
