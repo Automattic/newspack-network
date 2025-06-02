92a9af7ced46401e30cfe5c24ae9f5dccee025f8
matticbot <semantic-release-bot@martynus.net>
2025-06-02 19:35:10 +0000

    chore(release): 2.10.0 [skip ci]
    # [2.10.0](https://github.com/Automattic/newspack-network/compare/v2.9.0...v2.10.0) (2025-06-02)
    
    ### Features
    
    * add option to not create new terms on content distrib ([#238](https://github.com/Automattic/newspack-network/issues/238)) ([0c4772a](https://github.com/Automattic/newspack-network/commit/0c4772a4a8b8987b899f199e17d24530dba91276))
    * add support to yoast primary cat in content distribution ([#241](https://github.com/Automattic/newspack-network/issues/241)) ([be614cf](https://github.com/Automattic/newspack-network/commit/be614cfda1006797b9b812fe5ac650d0f90e97a6))
    * sync post modified date on post ditribution ([#240](https://github.com/Automattic/newspack-network/issues/240)) ([eb123ce](https://github.com/Automattic/newspack-network/commit/eb123ce9992a4feea16f307bc4cdebae9eb83291))

--- newspack-network.php
+++ newspack-network.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Network
  * Description: The Newspack Network plugin.
- * Version: 2.10.0-alpha.2
+ * Version: 2.10.0
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
