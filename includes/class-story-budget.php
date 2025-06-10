d385b8a200d42944e0fdda6cca45ae415cefb044
Miguel Peixe <miguel.peixe@automattic.com>
2025-06-10 13:51:53 -0300

    fix: add hub to site list and other tweaks

--- includes/class-story-budget.php
+++ includes/class-story-budget.php
@@ -9,6 +9,7 @@ namespace Newspack_Network;
 
 use Newspack_Network\Hub\Node;
 use Newspack_Network\Hub\Nodes;
+use Newspack_Network\Node\Settings as Node_Settings;
 use Newspack_Network\Content_Distribution;
 use Newspack_Network\Content_Distribution\Outgoing_Post;
 
@@ -23,7 +24,7 @@ class Story_Budget {
 		add_action( 'admin_enqueue_scripts', [ __CLASS__, 'enqueue_assets' ], 9 );
 		add_filter( 'rest_allowed_cors_headers', [ __CLASS__, 'add_cors_headers' ] );
 		add_filter( 'newspack_story_budget_fields', [ __CLASS__, 'add_fields' ] );
-		add_filter( 'newspack_story_budget_story_metadata', [ __CLASS__, 'add_story_metadata' ], 10, 2 );
+		add_filter( 'newspack_story_budget_story_metadata', [ __CLASS__, 'add_story_remote_metadata' ], 10, 2 );
 		add_filter( 'newspack_story_budget_fields_props', [ __CLASS__, 'add_fields_props' ], 10, 2 );
 	}
 
@@ -75,7 +76,8 @@ class Story_Budget {
 	 * @return array The fields to add.
 	 */
 	public static function add_fields( $fields ) {
-		if ( ! class_exists( '\Newspack_Network\Content_Distribution' ) ) {
+		$sites_list = self::get_sites_list();
+		if ( empty( $sites_list ) ) {
 			return $fields;
 		}
 
@@ -89,7 +91,7 @@ class Story_Budget {
 			'show_in_table'      => true,
 			'slug'               => 'network_sites',
 			'type'               => 'text',
-			'options'            => self::get_sites_list(),
+			'options'            => $sites_list,
 			'default_order'      => 18,
 			'get_value_callback' => [ __CLASS__, 'get_field_value' ],
 		];
@@ -105,12 +107,10 @@ class Story_Budget {
 	 * @return array The value of the field.
 	 */
 	public static function get_field_value( $post_id ) {
-		if ( class_exists( '\Newspack_Network\Content_Distribution' ) ) {
-			// If the post has been distributed, read the value from the distributed post.
-			$distributed_post = Content_Distribution::get_distributed_post( $post_id );
-			if ( $distributed_post ) {
-				return $distributed_post->get_distribution();
-			}
+		// If the post has been distributed, read the value from the distributed post.
+		$distributed_post = Content_Distribution::get_distributed_post( $post_id );
+		if ( $distributed_post ) {
+			return $distributed_post->get_distribution();
 		}
 
 		$field = \Newspack_Story_Budget\Fields::get_field( 'network_sites' );
@@ -124,9 +124,6 @@ class Story_Budget {
 	 */
 	private static function get_sites_list() {
 		$sites = [];
-		if ( ! class_exists( '\Newspack_Network\Content_Distribution' ) ) {
-			return $sites;
-		}
 
 		if ( Site_Role::is_hub() ) {
 			$nodes = Nodes::get_all_nodes();
@@ -139,6 +136,12 @@ class Story_Budget {
 		}
 
 		if ( Site_Role::is_node() ) {
+			$hub_url = Node_Settings::get_hub_url();
+			$sites[] = [
+				// The hub name is not stored in the node, so we'll use a pretty URL as label.
+				'label' => preg_replace( '#^https?://(www\.)?#', '', untrailingslashit( $hub_url ) ),
+				'value' => $hub_url,
+			];
 			$nodes_data = get_option( Node::HUB_NODES_SYNCED_OPTION, [] );
 			foreach ( $nodes_data as $node_data ) {
 				$sites[] = [
@@ -152,26 +155,28 @@ class Story_Budget {
 	}
 
 	/**
-	 * Add story metadata to the Newspack Story Budget.
+	 * Add metadata to stories when accessing from a different node.
 	 *
 	 * @param array $metadata The story metadata.
 	 * @param int   $story_id The story ID.
 	 *
 	 * @return array The story metadata.
 	 */
-	public static function add_story_metadata( $metadata, $story_id ) {
+	public static function add_story_remote_metadata( $metadata, $story_id ) {
 		if ( ! defined( 'REST_REQUEST' ) || ! REST_REQUEST ) {
+			return $metadata;
+		}
+
+		$request_site_url = filter_input( INPUT_SERVER, 'HTTP_X_NETWORK_SITE_URL', FILTER_VALIDATE_URL );
+
+		// Only modify the metadata if this is a remote request.
+		if ( ! $request_site_url ) {
 			return $metadata;
 		}
 
 		$metadata['can_pull']  = true;
 		$metadata['is_pulled'] = false;
 
-		$request_site_url = filter_input( INPUT_SERVER, 'HTTP_X_NETWORK_SITE_URL', FILTER_VALIDATE_URL );
-		if ( ! $request_site_url ) {
-			$metadata['can_pull'] = false;
-			return $metadata;
-		}
 		$outgoing_post = Content_Distribution::get_distributed_post( $story_id );
 		if ( ! $outgoing_post ) {
 			return $metadata;
