39a7d9f90a6c3d8754b33d3a8c6b6b97d21fdb57
Miguel Peixe <miguel.peixe@automattic.com>
2025-06-16 14:33:56 -0300

    feat(story-budget): set budget when pulling stories (#249)

---
+++ src/story-budget/components/local-budgets-control.js
@@ -0,0 +1,49 @@
+/**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import { useEffect, useState } from '@wordpress/element';
+import { SelectControl } from '@wordpress/components';
+import apiFetch from '@wordpress/api-fetch';
+
+export default function LocalBudgetsControl( {
+	value,
+	onChange,
+	disabled,
+	...props
+} ) {
+	const [ isLoading, setIsLoading ] = useState( false );
+	const [ localBudgets, setLocalBudgets ] = useState( [] );
+
+	useEffect( () => {
+		const fetchLocalBudgets = async () => {
+			setIsLoading( true );
+			const res = await apiFetch( {
+				path: 'newspack-story-budget/v1/budgets?include_archived=false',
+			} );
+			setLocalBudgets( res.budgets );
+			setIsLoading( false );
+		};
+		fetchLocalBudgets();
+	}, [] );
+
+	return (
+		<SelectControl
+			{ ...props }
+			disabled={ isLoading || disabled }
+			label={ __( 'Budget', 'newspack-network' ) }
+			value={ value }
+			onChange={ onChange }
+			options={ [
+				{
+					label: __( 'Select a budget', 'newspack-network' ),
+					value: '',
+				},
+				...localBudgets.map( budget => ( {
+					label: budget.name,
+					value: budget.id,
+				} ) ),
+			] }
+		/>
+	);
+}
