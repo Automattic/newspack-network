b69727f977ed55db0c1510b605dba80009a9dde0
matticbot <semantic-release-bot@martynus.net>
2025-07-09 19:32:52 +0000

    chore(release): 2.12.0-alpha.2 [skip ci]
    # [2.12.0-alpha.2](https://github.com/Automattic/newspack-network/compare/v2.12.0-alpha.1...v2.12.0-alpha.2) (2025-07-09)
    
    ### Bug Fixes
    
    * **content-distribution:** Distributor migration tweaks ([#259](https://github.com/Automattic/newspack-network/issues/259)) ([165b540](https://github.com/Automattic/newspack-network/commit/165b540f6eb6ddde9d24e5111b75c73e8000a5fa))

--- newspack-network.php
+++ newspack-network.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Network
  * Description: The Newspack Network plugin.
- * Version: 2.11.1
+ * Version: 2.12.0-alpha.2
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
