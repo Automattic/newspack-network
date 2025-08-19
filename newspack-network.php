1b1e746edc239862dbec585e15ac070cd29ba279
matticbot <semantic-release-bot@martynus.net>
2025-08-19 20:54:19 +0000

    chore(release): 2.14.2 [skip ci]
    ## [2.14.2](https://github.com/Automattic/newspack-network/compare/v2.14.1...v2.14.2) (2025-08-19)
    
    ### Bug Fixes
    
    * **content-distribution:** populate distributed featured image meta ([#272](https://github.com/Automattic/newspack-network/issues/272)) ([cc5388d](https://github.com/Automattic/newspack-network/commit/cc5388d255e9581cea59915313ea860d11fc4b6e))

--- newspack-network.php
+++ newspack-network.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack Network
  * Description: The Newspack Network plugin.
- * Version: 2.14.1
+ * Version: 2.14.2
  * Author: Automattic
  * Author URI: https://newspack.com/
  * License: GPL3
