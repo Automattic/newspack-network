dea45725b357fc9a898ee997f6420b808019d6fe
leogermani <leogermani@gmail.com>
2026-02-02 11:49:58 -0300

    docs: document constants

--- newspack-network.php
+++ newspack-network.php
@@ -15,12 +15,12 @@
 
 defined( 'ABSPATH' ) || exit;
 
-// Define NEWSPACK_NETWORK_PLUGIN_DIR.
+// Path to the Newspack Network plugin directory.
 if ( ! defined( 'NEWSPACK_NETWORK_PLUGIN_DIR' ) ) {
 	define( 'NEWSPACK_NETWORK_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 
-// Define NEWSPACK_NETWORK_PLUGIN_FILE.
+// Path to the main Newspack Network plugin file.
 if ( ! defined( 'NEWSPACK_NETWORK_PLUGIN_FILE' ) ) {
 	define( 'NEWSPACK_NETWORK_PLUGIN_FILE', __FILE__ );
 }
